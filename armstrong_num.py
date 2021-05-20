e167ad40abc391b236a318b5d796d7f19c901f42
sainadhareddy <65475797+sainadhareddy@users.noreply.github.com>
2021-05-20 10:10:44 +0530

    Update armstrong_num.py

--- armstrong_num.py
+++ armstrong_num.py
@@ -1,14 +1,14 @@
-num = int(input("Enter a number: "))
+num = int(input('Enter a number : '))
+num1, num2 = num, num
 sum = 0
-# find the sum of the cube of each digit
-temp = num
-while temp > 0:
-    result = temp % 10
-    sum += result ** 3
-    temp //= 10
-
-# display the result
-if num == sum:
-    print(num, "is an Armstrong number")
+while num:
+    sum += 1
+    num //= 10
+temp = 0
+while num2:
+    temp = temp + pow(num2 % 10, sum)
+    num2 //= 10
+if num1 != temp:
+    print('Entered number is not Armstrong')
 else:
-    print(num, "is not an Armstrong number")
+    print('Entered number is an Armstrong')
