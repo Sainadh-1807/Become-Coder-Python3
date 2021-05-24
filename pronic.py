0a4e7e93897cdd084a3925ae87a928e25ff4951b
sainadhareddy <65475797+sainadhareddy@users.noreply.github.com>
2021-05-24 08:56:05 +0530

    Add files via upload

---
+++ pronic.py
@@ -0,0 +1,17 @@
+import math
+
+
+def pronic_check(n):
+    x = (int)(math.sqrt(n))
+    if (x * (x + 1) == n):
+        return True
+    else:
+        return False
+
+
+num = int(input('Enter a number : '))
+
+if (pronic_check(num) == True):
+    print(num, "is a pronic number")
+else:
+    print(num, "is not a pronic number")
