90ab688a412748930d5b55bf18e81c3de1e105e7
sainadhareddy <65475797+sainadhareddy@users.noreply.github.com>
2021-05-29 09:10:25 +0530

    Add files via upload

---
+++ revpattern7.py
@@ -0,0 +1,10 @@
+row_count = int(input("Enter number of rows : "))
+
+for i in range(1, row_count+1):
+    for j in range(1, i+1):
+        if j % 2 == 0:
+            print('0', end=" ")
+        else:
+            print('1', end=" ")
+
+    print("\n")
